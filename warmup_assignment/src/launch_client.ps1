ab7ef76e985cc24ad3667e9430984b1e921df6fa
Riccardo Cannistrà <rickystream94@gmail.com>
2018-02-09 14:31:03 +0100

    Pushed essential files

---
+++ warmup_assignment/src/launch_client.ps1
@@ -0,0 +1,12 @@
+param (
+    [Parameter(Mandatory=$true)][string]$LevelName,
+    [string]$SearchType = "bfs",
+    [int]$MillisPerGuiAction = 50,
+    [int]$TimeoutInSeconds = 180
+ )
+
+ # Recompile classes
+javac searchclient/*.java
+
+# Launch server + search client
+java -jar server.jar -l "levels/$LevelName.lvl" -c "java -Xmx4g searchclient.SearchClient -$SearchType" -g $MillisPerGuiAction -t $TimeoutInSeconds
