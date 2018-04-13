ce62223cddde146393f1a791c434832888f337f8
yago.rosario@hotmail.com.br <Alzira7966>
2018-04-13 16:23:40 +0200

    Creating new branch

--- sokoban/src/architecture/BDIManager.java
+++ sokoban/src/architecture/BDIManager.java
@@ -1,4 +1,42 @@
 package architecture;
 
+import board.Agent;
+import board.Coordinate;
+import board.Level;
+import planning.actions.CompoundTask;
+import server.action.Action;
+import utils.FibonacciHeap;
+import utils.FibonacciHeap.Entry;
+
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+
 public class BDIManager {
+
+    private Level level;
+    private AbstractMap<Agent, FibonacciHeap<CompoundTask>> actionsByAgent;
+
+    public BDIManager(Level level) {
+        this.level = level;
+        actionsByAgent = new HashMap<>();
+    }
+
+    public CompoundTask getCompoundTaskByAgent(Agent agent) {
+        FibonacciHeap<CompoundTask> compoundTaskHeap = actionsByAgent.get(agent);
+
+        if(compoundTaskHeap != null) {
+            Entry<CompoundTask> compoundTask = compoundTaskHeap.dequeueMin();
+            if(compoundTask != null)
+                return compoundTask.getValue();
+        }
+        
+        return null;
+    }
+
+    //TODO Create a Heuristic Class
+    public int ManhattanDistance(Coordinate c0, Coordinate c1) {
+        return Math.abs(c0.getCol() - c0.getCol()) + Math.abs(c0.getRow() - c0.getRow());
+    }
+
 }
