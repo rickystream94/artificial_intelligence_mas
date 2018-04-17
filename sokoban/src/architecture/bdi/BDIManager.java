1b474bcfcc78b42b9fc9ab6178be1418ea73f2ec
Riccardo Cannistrà <rickystream94@gmail.com>
2018-04-17 19:26:07 +0200

    BDIManager now generates desires and (semi) optimally assigns them to the agents

--- sokoban/src/architecture/bdi/BDIManager.java
+++ sokoban/src/architecture/bdi/BDIManager.java
@@ -2,153 +2,123 @@ package architecture.bdi;
 
 import architecture.ClientManager;
 import board.*;
-import javafx.util.Pair;
-import planning.actions.CompoundTask;
+import logging.ConsoleLogger;
 import utils.FibonacciHeap;
-import utils.FibonacciHeap.Entry;
 
 import java.util.*;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class BDIManager {
 
+    private static final Logger LOGGER = ConsoleLogger.getLogger(BDIManager.class.getSimpleName());
+
     public Map<Agent, FibonacciHeap<Desire>> generateDesires() {
-        // Prepare
-        Map<Agent, FibonacciHeap<Desire>> actionsByAgent = new HashMap<>();
+        ConsoleLogger.logInfo(LOGGER, "Generating desires...");
+
+        // Step 0: Prepare
+        Map<Agent, FibonacciHeap<Desire>> agentDesiresMap = new HashMap<>();
         Level level = ClientManager.getInstance().getLevelManager().getLevel();
         Set<Agent> agents = new HashSet<>(level.getAgents());
         Set<Goal> goals = new HashSet<>(Level.getGoals());
         Set<Box> boxes = new HashSet<>(level.getBoxes());
+        List<Desire> desires = new ArrayList<>();
+        agents.forEach(agent -> agentDesiresMap.put(agent, new FibonacciHeap<>()));
 
-        // Map each type to the list of boxes/goals belonging to that type
+        // Step 1: Map each type to the list of boxes/goals belonging to that type
         Map<Character, List<Box>> boxesPerTypeMap = new HashMap<>();
         Map<Character, List<Goal>> goalsPerTypeMap = new HashMap<>();
         for (Box box : boxes) {
-            if (!boxesPerTypeMap.containsKey(box.getBoxType()))
-                boxesPerTypeMap.put(box.getBoxType(), new ArrayList<>());
-            List<Box> boxesOfType = boxesPerTypeMap.get(box.getBoxType());
-            boxesOfType.add(box);
-            boxesPerTypeMap.put(box.getBoxType(), boxesOfType);
+            char boxType = Character.toLowerCase(box.getBoxType());
+            if (!boxesPerTypeMap.containsKey(boxType))
+                boxesPerTypeMap.put(boxType, new ArrayList<>());
+            boxesPerTypeMap.get(boxType).add(box);
         }
         for (Goal goal : goals) {
-            if (!goalsPerTypeMap.containsKey(goal.getGoalType()))
-                goalsPerTypeMap.put(goal.getGoalType(), new ArrayList<>());
-            List<Goal> goalsOfType = goalsPerTypeMap.get(goal.getGoalType());
-            goalsOfType.add(goal);
-            goalsPerTypeMap.put(goal.getGoalType(), goalsOfType);
+            char goalType = goal.getGoalType();
+            if (!goalsPerTypeMap.containsKey(goalType))
+                goalsPerTypeMap.put(goalType, new ArrayList<>());
+            goalsPerTypeMap.get(goalType).add(goal);
         }
+        ConsoleLogger.logInfo(LOGGER, "Step 1: Done mapping boxes and goals to their type");
 
-        // Step 1: Box-Goal assignment --> Which box goes to which goal? --> Output = List of Desires
-        List<Desire> desires = new ArrayList<>();
+        // Step 2: Box-Goal assignment --> Which box goes to which goal? --> Output = List of Desires
         for (Character type : goalsPerTypeMap.keySet()) {
-            List<Box> boxesOfType = boxesPerTypeMap.get(type);
-            List<Goal> goalsOfType = goalsPerTypeMap.get(type);
             // Create a desire for each goal
-            while (!goalsOfType.isEmpty()) {
-                Map<Desire, Integer> desiresCost = new HashMap<>();
-                for (Box box : boxesOfType) {
-                    for (Goal goal : goalsOfType) {
-                        int cost = Coordinate.manhattanDistance(box.getCoordinate(), goal.getCoordinate());
-                        desiresCost.put(new Desire(box, goal), cost);
+            while (!goalsPerTypeMap.get(type).isEmpty()) {
+                Map<Object, Integer> desireCostMap = new HashMap<>();
+                for (Box box : boxesPerTypeMap.get(type)) {
+                    for (Goal goal : goalsPerTypeMap.get(type)) {
+                        int cost = getCostBetweenObjects(box, goal);
+                        desireCostMap.put(new Desire(box, goal), cost);
                     }
                 }
-                Desire minCostDesire = getKeyByMinValue(desiresCost);
+                Desire minCostDesire = (Desire) getKeyByMinValue(desireCostMap);
                 desires.add(minCostDesire);
 
                 // Remove occurrence of chosen box and goal for the current desire
-                boxesOfType.remove(minCostDesire.getBox());
-                goalsOfType.remove(minCostDesire.getGoal());
-                boxesPerTypeMap.put(type, boxesOfType);
-                goalsPerTypeMap.put(type, goalsOfType);
+                boxesPerTypeMap.get(type).remove(minCostDesire.getBox());
+                goalsPerTypeMap.get(type).remove(minCostDesire.getGoal());
             }
         }
         // Testing purposes --> All goals should be assigned to a box (there can be, instead, a box not assigned to any goal)
         assert goalsPerTypeMap.values().stream().allMatch(List::isEmpty);
+        ConsoleLogger.logInfo(LOGGER, "Step 2: Done creating desires as Box-Goal mappings");
 
-        // Step 2: Desire assignment to agents
-        for (Agent agent : agents) {
-            List<Box> boxesForAgent = agentAndBoxesMap.get(agent);
-            for (Box box : boxesForAgent) {
-                if (!boxAndGoalsMap.containsKey(box))
-                    continue;
-                List<Goal> goalsForBox = boxAndGoalsMap.get(box);
-                for (Goal goal : goalsForBox) {
-                    double dist = getActionCost(agent, box, goal);
-                    Pair<Box, Goal> action = new Pair<>(box, goal);
-                    if (!actionsByAgent.containsKey(action)) {
-                        actionsByAgent.put(action, new Pair<>(agent, dist));
-                    } else {
-                        Pair<Agent, Double> responsible = actionsByAgent.get(action);
-                        double cost = responsible.getValue();
-                        if (cost > dist) {
-                            actionsByAgent.put(action, new Pair<>(agent, dist));
-                        }
-                    }
-                }
+        // Step 3: Create support data structure to keep track of workload of each agent
+        Map<Color, Map<Object, Integer>> workloadOfAgentsByColor = new HashMap<>();
+        for (Color c : agents.stream().map(Agent::getColor).collect(Collectors.toList())) {
+            workloadOfAgentsByColor.put(c, new HashMap<>());
+            agents.stream().filter(agent -> agent.getColor() == c).forEach(agent -> workloadOfAgentsByColor.get(c).put(agent, 0));
+        }
+        ConsoleLogger.logInfo(LOGGER, "Step 3: Done mapping boxes and goals to their type");
+
+        // Step 4: Assign Desires  to agents keeping the workload well spread among them
+        while (!desires.isEmpty()) {
+            Desire currentDesire = desires.remove(0);
+            List<Agent> agentsOfColor = agents.stream().filter(agent -> agent.getColor() == currentDesire.getBox().getColor()).collect(Collectors.toList());
+            Map<Object, Integer> desireCostByAgent = new HashMap<>(); // Each agent's cost to reach the desire's box
+            agentsOfColor.forEach(agent -> desireCostByAgent.put(agent, getCostBetweenObjects(agent, currentDesire.getBox())));
+            Agent chosenAgent = (Agent) getKeyByMinValue(desireCostByAgent);
+
+            // Check if current workload of chosen agent is above the minimum
+            Map<Object, Integer> agentsWorkload = workloadOfAgentsByColor.get(currentDesire.getBox().getColor());
+            if (agentsWorkload.get(chosenAgent) > agentsWorkload.values().stream().min(Integer::compareTo).get()) {
+                // Desire is assigned to the agent with the lowest workload
+                chosenAgent = (Agent) getKeyByMinValue(agentsWorkload);
             }
-        }
 
-        for (Map.Entry<Pair<Box, Goal>, Pair<Agent, Double>> entry : actionsByAgent.entrySet()) {
-            Agent agent = entry.getValue().getKey();
-            Double cost = entry.getValue().getValue();
-            if (!this.actionsByAgent.containsKey(agent))
-                this.actionsByAgent.put(agent, new FibonacciHeap<>());
-            FibonacciHeap<Pair<Box, Goal>> actions = this.actionsByAgent.get(agent);
-            actions.enqueue(entry.getKey(), cost);
-        }
-    }
+            // Increment workload of chosen agent
+            agentsWorkload.put(chosenAgent, agentsWorkload.get(chosenAgent) + 1);
 
-    private Desire getKeyByMinValue(Map<Desire, Integer> desiresCost) {
-        int minValue = Integer.MAX_VALUE;
-        Desire chosenDesire = null;
-        for (Desire desire : desiresCost.keySet()) {
-            int value = desiresCost.get(desire);
-            if (value < minValue) {
-                minValue = value;
-                chosenDesire = desire;
-            }
+            // Assign desire to chosen agent
+            agentDesiresMap.get(chosenAgent).enqueue(currentDesire, (double) desireCostByAgent.get(chosenAgent));
         }
-        return chosenDesire;
-    }
+        ConsoleLogger.logInfo(LOGGER, "Step 4: Done (semi-optimally) assigning desires to agents");
 
-    public CompoundTask getCompoundTaskByAgent(Agent agent) {
-        /*
-        FibonacciHeap<CompoundTask> compoundTaskHeap = actionsByAgent.get(agent);
-
-        if(compoundTaskHeap != null) {
-            Entry<CompoundTask> compoundTask = compoundTaskHeap.dequeueMin();
-            if(compoundTask != null)
-                return compoundTask.getValue();
-        }
-        */
-        return null;
+        return agentDesiresMap;
     }
 
     /**
-     * This function fucks up the FibonacciHeap
+     * Given a map with generic keys and integer values, returns the key that has the minimum value
+     *
+     * @param map Generic Object-Integer map
+     * @return key corresponding to minimum value
      */
-    private void printActions() {
-        for (Map.Entry<Agent, FibonacciHeap<Pair<Box, Goal>>> entry : this.actionsByAgent.entrySet()) {
-            Agent agent = entry.getKey();
-            FibonacciHeap<Pair<Box, Goal>> actions = entry.getValue();
-
-            while (!actions.isEmpty()) {
-                Entry<Pair<Box, Goal>> actionEntry = actions.dequeueMin();
-
-                double cost = actionEntry.getPriority();
-                Pair<Box, Goal> action = actionEntry.getValue();
-                if (action == null) continue;
-
-                Goal goal = action.getValue();
-                Box box = action.getKey();
-
-                System.err.println(box + " " + goal + " " + agent + " " + cost);
+    private Object getKeyByMinValue(Map<Object, Integer> map) {
+        int minValue = Integer.MAX_VALUE;
+        Object chosenKey = null;
+        for (Object key : map.keySet()) {
+            int value = map.get(key);
+            if (value < minValue) {
+                minValue = value;
+                chosenKey = key;
             }
         }
+        return chosenKey;
     }
 
-    private double getActionCost(Agent agent, Box box, Goal goal) {
-        double dist = ManhattanDistance(box.getCoordinate(), goal.getCoordinate());
-        dist += ManhattanDistance(agent.getCoordinate(), box.getCoordinate());
-        return dist;
+    private int getCostBetweenObjects(SokobanObject o1, SokobanObject o2) {
+        return Coordinate.manhattanDistance(o1.getCoordinate(), o2.getCoordinate());
     }
 }
